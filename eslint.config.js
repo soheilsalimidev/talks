d670fa2831ea9edbd1678b174f44b7e5a8eb4867
Soheil Salimi <soheilsalimidev@gmail.com>
2024-12-22 20:04:00 +0330

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  react: true,
+  formatters: {
+    css: true,
+    // markdown: true,
+    // slidev: {
+    //   files: [
+    //     '*/src/slides.md',
+    //   ],
+    // },
+  },
+  ignores: [
+    '**/demo/eslint/**',
+  ],
+})
